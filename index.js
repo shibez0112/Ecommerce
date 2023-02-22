7e114c41c8cb980d047bff574fa590878e5082cd
shibez <ginta2888@gmail.com>
2023-02-22 15:15:38 +0700

    feat: Add user model and register feature

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
+const bodyParser = require("body-parser");
 const express = require("express");
 const dbConnect = require("./config/dbConnect");
 const app = express();
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 3000;
-dbConnect();
-app.use("/", (req, res) => {
-  res.send("Hello from server side");
-});
+const authRouter = require("./routes/authRoute");
+// dbConnect();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
 
+app.use("/api/user", authRouter);
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 module.exports = app;
