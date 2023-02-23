0f53f5d084de8e0f8a8d844968e7a518d3095933
shibez <ginta2888@gmail.com>
2023-02-23 14:41:39 +0700

    feat: Add new middleware
    Add middleware for authenticate the token given when log in and check role if user is admin

--- models/userModel.js
+++ models/userModel.js
@@ -2,44 +2,55 @@ const mongoose = require("mongoose"); // Erase if already required
 const bcrypt = require("bcrypt");
 
 // Declare the Schema of the Mongo model
-var userSchema = new mongoose.Schema({
-  firstname: {
-    type: String,
-    required: true,
-    unique: true,
+var userSchema = new mongoose.Schema(
+  {
+    firstname: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    lastname: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    mobile: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      default: "user",
+    },
+    cart: {
+      type: Array,
+      default: [],
+    },
+    address: [{ type: mongoose.Schema.Types.ObjectId, ref: "Address" }],
+    wishlist: [{ type: mongoose.Schema.Types.ObjectId, ref: "Product" }],
   },
-  lastname: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  mobile: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  role: {
-    type: String,
-    default: "user",
+  {
+    timestamps: true,
   }
-});
+);
 
-userSchema.pre("save", async function(next){
-    const salt = bcrypt.genSaltSync(10);
-    this.password = await bcrypt.hash(this.password, salt);
+userSchema.pre("save", async function (next) {
+  const salt = bcrypt.genSaltSync(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 userSchema.methods.isPasswordMatched = async function (enteredPassword) {
-    return await bcrypt.compare(enteredPassword, this.password);
-}
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 //Export the model
 module.exports = mongoose.model("User", userSchema);
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,11 +1,19 @@
 const express = require("express");
-const { createUser, loginUserCtrl, getAllUser, getaUser, deleteaUser, updateaUser } = require("../controller/userCtrl");
+const {
+  createUser,
+  loginUserCtrl,
+  getAllUser,
+  getaUser,
+  deleteaUser,
+  updateaUser,
+} = require("../controller/userCtrl");
+const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware");
 const router = express.Router();
 
 router.post("/register", createUser);
 router.post("/login", loginUserCtrl);
 router.get("/all-users", getAllUser);
-router.get("/:id", getaUser);
+router.get("/:id", authMiddleware, isAdmin, getaUser);
 router.delete("/:id", deleteaUser);
 router.patch("/:id", updateaUser);
 
