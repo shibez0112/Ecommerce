572f6123f48a361178e9a7cedae1c5c79fbf4c1a
shibez <ginta2888@gmail.com>
2023-02-24 22:24:43 +0700

    test: Add test for create, get by id and get all product

---
+++ test/productRoute.test.js
@@ -0,0 +1,100 @@
+const mongoose = require("mongoose");
+const request = require("supertest");
+const app = require("../index");
+const { MongoMemoryServer } = require("mongodb-memory-server");
+
+describe("Testing Product Route API", () => {
+  let cookies;
+  let productId;
+
+  beforeAll(async () => {
+    const mongoServer = await MongoMemoryServer.create();
+
+    await mongoose.connect(mongoServer.getUri());
+  });
+
+  afterAll(async () => {
+    await mongoose.disconnect();
+    await mongoose.connection.close();
+  });
+
+  it("POST /api/product/ Create a new product", async () => {
+    const mockProduct = {
+      title: "Apple Watch",
+      slug: "abc",
+      description: "This is an apple product",
+      price: 500,
+      quantity: 100
+    };
+
+    const res = await request(app)
+      .post("/api/product/")
+      .set("Content-type", "application/json")
+      .send(mockProduct)
+      .expect(200);
+    expect(res.body).toEqual(
+      expect.objectContaining({
+        title: "Apple Watch",
+        slug: "abc",
+        description: "This is an apple product",
+        quantity: 100,
+      })
+    );
+  });
+
+
+  it("POST /api/product/ Create a second product", async () => {
+    const mockProduct = {
+      title: "Apple Watch 2",
+      slug: "abcd",
+      description: "This is a new apple product",
+      price: 1500,
+      quantity: 1100
+    };
+
+    const res = await request(app)
+      .post("/api/product/")
+      .set("Content-type", "application/json")
+      .send(mockProduct)
+      .expect(200);
+    expect(res.body).toEqual(
+      expect.objectContaining({
+      title: "Apple Watch 2",
+      slug: "abcd",
+      description: "This is a new apple product",
+      price: 1500,
+      quantity: 1100
+      })
+    );
+
+      productId = res.body._id;
+  });
+
+  it("GET /api/product/:id Get a product", async () => {
+    const res = await request(app)
+      .get(`/api/product/${productId}`)
+      .set("Content-type", "application/json")
+      .expect(200);
+    expect(res.body).toEqual(
+      expect.objectContaining({
+      title: "Apple Watch 2",
+      slug: "abcd",
+      description: "This is a new apple product",
+      price: 1500,
+      quantity: 1100
+      })
+    );
+  });
+
+  it("GET /api/product/all-product Get all product", async () => {
+    const res = await request(app)
+      .get("/api/product/all-product")
+      .set("Content-type", "application/json")
+      .expect(200);
+    expect(res.body.length).toBeGreaterThan(0);
+    console.log(res.body);
+  });
+
+
+
+});
