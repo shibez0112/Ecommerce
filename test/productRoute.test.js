4c72b87fccc97d517feaeaa2ee6216edbd24e30c
shibez <ginta2888@gmail.com>
2023-02-25 21:49:50 +0700

    test: Fix test on product route
    Add new property to mock data to make test usable
    
    Test unable to run when update new data model

--- test/productRoute.test.js
+++ test/productRoute.test.js
@@ -24,7 +24,10 @@ describe("Testing Product Route API", () => {
       slug: "abc",
       description: "This is an apple product",
       price: 500,
-      quantity: 100
+      quantity: 100,
+      category: "Phone",
+      brand: "Apple",
+      color: "Red",
     };
 
     const res = await request(app)
@@ -38,10 +41,12 @@ describe("Testing Product Route API", () => {
         slug: "abc",
         description: "This is an apple product",
         quantity: 100,
+        category: "Phone",
+        brand: "Apple",
+        color: "Red",
       })
     );
   });
-
 
   it("POST /api/product/ Create a second product", async () => {
     const mockProduct = {
@@ -49,7 +54,10 @@ describe("Testing Product Route API", () => {
       slug: "abcd",
       description: "This is a new apple product",
       price: 1500,
-      quantity: 1100
+      quantity: 1100,
+      category: "Phone",
+      brand: "Apple",
+      color: "Red",
     };
 
     const res = await request(app)
@@ -59,15 +67,18 @@ describe("Testing Product Route API", () => {
       .expect(200);
     expect(res.body).toEqual(
       expect.objectContaining({
-      title: "Apple Watch 2",
-      slug: "abcd",
-      description: "This is a new apple product",
-      price: 1500,
-      quantity: 1100
+        title: "Apple Watch 2",
+        slug: "abcd",
+        description: "This is a new apple product",
+        price: 1500,
+        quantity: 1100,
+        category: "Phone",
+        brand: "Apple",
+        color: "Red",
       })
     );
 
-      productId = res.body._id;
+    productId = res.body._id;
   });
 
   it("GET /api/product/:id Get a product", async () => {
@@ -77,11 +88,14 @@ describe("Testing Product Route API", () => {
       .expect(200);
     expect(res.body).toEqual(
       expect.objectContaining({
-      title: "Apple Watch 2",
-      slug: "abcd",
-      description: "This is a new apple product",
-      price: 1500,
-      quantity: 1100
+        title: "Apple Watch 2",
+        slug: "abcd",
+        description: "This is a new apple product",
+        price: 1500,
+        quantity: 1100,
+        category: "Phone",
+        brand: "Apple",
+        color: "Red",
       })
     );
   });
@@ -92,9 +106,6 @@ describe("Testing Product Route API", () => {
       .set("Content-type", "application/json")
       .expect(200);
     expect(res.body.length).toBeGreaterThan(0);
-    console.log(res.body);
+//    console.log(res.body);
   });
-
-
-
 });
