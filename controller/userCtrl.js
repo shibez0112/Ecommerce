c1226ba7f8a28d2084387ae62daf64bec6b481ce
shibez <ginta2888@gmail.com>
2023-03-28 17:27:19 +0700

    feat: Add login admin route

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -408,6 +408,37 @@ const getOrders = asyncHandler(async (req, res) => {
   }
 });
 
+const loginAdmin = asyncHandler(async(req, res)=>{
+  const { email, password } = req.body;
+  // check if user exists or not
+  const findAdmin = await User.findOne({ email });
+  if (findAdmin.role !== "admin") throw new Error("Not Authorized");
+  if (findAdmin && (await findAdmin.isPasswordMatched(password))) {
+    const refreshToken = await generateRefreshToken(findAdmin?.id);
+    const updateUser = await User.findByIdAndUpdate(
+      findAdmin.id,
+      {
+        refreshToken: refreshToken,
+      },
+      { new: true }
+    );
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      maxAge: 72 * 60 * 60 * 1000,
+    });
+    res.json({
+      _id: findAdmin?._id,
+      firstname: findAdmin?.firstname,
+      lastname: findAdmin?.lastname,
+      email: findAdmin?.email,
+      mobile: findAdmin?.mobile,
+      token: generateToken(findAdmin?.id),
+    });
+  } else {
+    throw new Error("Invalid Credentials");
+  }
+});
+
 module.exports = {
   createUser,
   loginUserCtrl,
@@ -430,4 +461,5 @@ module.exports = {
   applyCoupon,
   createOrder,
   getOrders,
+  loginAdmin
 };
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -21,12 +21,14 @@ const {
   applyCoupon,
   createOrder,
   getOrders,
+  loginAdmin,
 } = require("../controller/userCtrl");
 const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware");
 const router = express.Router();
 
 router.post("/register", createUser);
 router.post("/login", loginUserCtrl);
+router.post("/admin-login", loginAdmin);
 router.post("/forgot-password", forgotPasswordToken);
 router.post("/cart/apply-coupon", authMiddleware, applyCoupon);
 router.post("/cart/cash-order", authMiddleware, createOrder);
